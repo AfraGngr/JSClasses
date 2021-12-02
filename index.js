676707b5e06398dd7ace39241ef2790bee78e412
RosaStone <gungorafra93@gmail.com>
2021-12-02 22:11:35 +0300

    first session

---
+++ index.js
@@ -0,0 +1,86 @@
+// console.log("Hello World !")
+
+// //console.log("1", userName)
+
+// var user_Name = "Gamze"
+
+// userName = "Ebuzer"
+
+// console.log(userName)
+
+// let mail = "abc@gmail.com"
+// //let mail = "abcdef"
+
+// mail = "xyz"
+// console.log(mail)
+// //let -> mutable
+
+// mail = "xyz@mail.com"
+
+
+// console.log(mail)
+
+// const email = "ababab"
+// //email = "xyzasd"
+// //const -> immutable
+
+// // console.log(uName)
+// let uName="Afra";
+// //const secondName;
+
+// let firstUserName;
+// let s1Name;
+
+// let _name;
+// let $name;
+
+// let a = '3';
+// let b = 8;
+// let c = a
+
+// a = b ;
+// b = c ;
+
+// console.log(a, b)
+/* 
+This is used for 
+multiline comments
+*/
+
+
+// ------ DATA TYPES ------- //
+/* 
+    1. string; 
+    2. number; 
+    3. boolean; 
+    4. undefined; 
+    5. null; 
+*/
+
+let name = "Afra"
+
+
+let phoneNumber;
+console.log("phoneNumber", phoneNumber)
+let email = null;
+console.log("email", email)
+
+let variable = 'ali29';
+console.log(typeof variable)
+
+console.log(phoneNumber)
+console.log(email)
+
+console.log(typeof name)
+
+console.log(typeof 29)
+console.log(typeof "125")
+console.log(typeof 3.14)
+console.log(typeof "true")
+console.log(typeof true)
+console.log(typeof false)
+
+
+
+
+
