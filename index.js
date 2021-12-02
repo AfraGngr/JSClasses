f6c7e1c0bb864cc057d6a9a1b5d5834e7bf638fc
RosaStone <gungorafra93@gmail.com>
2021-12-02 22:52:13 +0300

    Basic Operators

--- index.js
+++ index.js
@@ -48,7 +48,14 @@ multiline comments
 */
 
 
-// ------ DATA TYPES ------- //
+var fName = "Afra"
+fname = "Abcdcd"
+let fName2 = "Muhlise"
+fName2 = "Kemal"
+const fName3 =  "Fatih"
+
+// ------------------------------------ DATA TYPES -------------------------------- //
+
 /* 
     1. string; 
     2. number; 
@@ -57,30 +64,91 @@ multiline comments
     5. null; 
 */
 
-let name = "Afra"
-
-
-let phoneNumber;
-console.log("phoneNumber", phoneNumber)
-let email = null;
-console.log("email", email)
-
-let variable = 'ali29';
-console.log(typeof variable)
-
-console.log(phoneNumber)
-console.log(email)
-
-console.log(typeof name)
-
-console.log(typeof 29)
-console.log(typeof "125")
-console.log(typeof 3.14)
-console.log(typeof "true")
-console.log(typeof true)
-console.log(typeof false)
+// let name;
+// console.log("name: " + name)
+// name = null; 
+// console.log("name", name)
 
 
 
+// let phoneNumber;
+// console.log("phoneNumber", phoneNumber)
+// let email = null;
+// console.log("email", email)
+
+// let variable = 'ali29';
+// console.log(typeof variable)
+
+// console.log(phoneNumber)
+// console.log(email)
+
+// console.log(typeof name)
+
+// console.log("11111",typeof 29)
+// console.log(typeof "125")
+// console.log("2222", typeof 3.14)
+// console.log(typeof "true")
+// console.log(typeof true)
+// console.log(typeof false)
+
+
+// ------------------------------------ BASIC OPERATORS  -------------------------------- //
+
+let currentYear = 2021;
+let age = currentYear - 1971;
+
+console.log("age: ", age)
+
+let a = 3;
+let b = 8;
+
+let total = a + b ;
+let totalValue =  5 + 8;
+console.log(total, totalValue)
+
+let x = 3 * 5 ;
+console.log(x)
+
+let y = 8 / 4;
+console.log(y)
+
+let exponent = 2 ** 3;
+console.log(exponent)
+
+let mod = 10 % 3;
+console.log(mod)
+
+
+// Increment, decrement
+
+// " -- " -> 1 azalt
+// " ++ " -> 1 artır
+
+let ab = 3; 
+let xy = --ab;   // bu satırda ab = 2  // azalt sonra ata 
+console.log(ab, xy) // bu satırda ab = 2
+
+let abc = 3;
+let xyz = abc-- // bu satırda abc = 3  // ata sonra azalt
+console.log(xyz, abc)
+
+
+let dec = 5;
+let assg = ++dec;  // artır sonra ata 
+
+console.log(dec, assg)
+
+let dec2 = 5;
+let assg2 = dec2++ // ata sonra artır
+
+console.log(assg2, dec2)
+
+
+// String Concetanation 
+
+const firstName = "Jeff";
+const surName = "Bezos";
+
+console.log(firstName + " " +surName)
 
 
