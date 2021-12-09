0b49e50a792677b4920d04b67e5aa7b7a927cc03
RosaStone <gungorafra93@gmail.com>
2021-12-09 22:23:15 +0300

    string methods

--- index.js
+++ index.js
@@ -542,105 +542,267 @@ Test data:
 
 // Piece of code that we can use multiple times
 
-function myFirstFunction(){
-    console.log("Hello World !")
-}
+// function myFirstFunction(){
+//     console.log("Hello World !")
+// }
 
-myFirstFunction() // call, execute, , invoke
-// myFirstFunction()
-// myFirstFunction()
-// myFirstFunction()
-// myFirstFunction()
+// myFirstFunction() // call, execute, , invoke
+// // myFirstFunction()
+// // myFirstFunction()
+// // myFirstFunction()
+// // myFirstFunction()
 
-function juiceExtractor (fruit1, fruit2){
-    // console.log("çalıştı")
-    const juice = `This is ${fruit1} and ${fruit2} juice.`
-    console.log("Function: ", juice)
-    return juice
-    // console.log(juice)
-}
-//console.log(juiceExtractor("apple", "orange")) 
-const myJuice = juiceExtractor("apple", "orange")
-console.log(myJuice)
+// function juiceExtractor (fruit1, fruit2){
+//     // console.log("çalıştı")
+//     const juice = `This is ${fruit1} and ${fruit2} juice.`
+//     console.log("Function: ", juice)
+//     return juice
+//     // console.log(juice)
+// }
+// //console.log(juiceExtractor("apple", "orange")) 
+// const myJuice = juiceExtractor("apple", "orange")
+// console.log(myJuice)
 
-//---- Function decleration 
-function calcAge (birthYear){
-    // const age = 2021 - birthYear
-    // return age
-    return 2021 - birthYear
-}
+// //---- Function decleration 
+// function calcAge (birthYear){
+//     // const age = 2021 - birthYear
+//     // return age
+//     return 2021 - birthYear
+// }
 
-//console.log(calcAge(1965))
+// //console.log(calcAge(1965))
 
-// console.log(calcAge(1997))
+// // console.log(calcAge(1997))
 
-//---- Function Expression code
-const addition = function (a, b){
-    return a + b
-}
+// //---- Function Expression code
+// const addition = function (a, b){
+//     return a + b
+// }
 
-const total = addition(1965, 1612737)
-console.log(total)
-
-
-// ---- Arrow Function 
-
-const calcAge2 = birthYear  => 2021 - birthYear 
-
-console.log(calcAge2(2000))
-
-const canDrive = (birthYear) => {
-    const age = 2021 - birthYear
-    const allowed = 18 - age
-    return `You can drive ${allowed} year later.`
-}
-
-const result = canDrive(2006)
-console.log(result)
+// const total = addition(1965, 1612737)
+// console.log(total)
 
 
-function hipotenus (a, b){
-    const firstValue = exponentValues(a)
-    const secondValue = exponentValues(b)
-    const result = (firstValue + secondValue) ** (0.5)
-    return result 
-}
+// // ---- Arrow Function 
 
-function exponentValues(value){
-    return value ** 2
-}
+// const calcAge2 = birthYear  => 2021 - birthYear 
 
-const hipoResult = hipotenus (7, 24)
-console.log(hipoResult)
+// console.log(calcAge2(2000))
+
+// const canDrive = (birthYear) => {
+//     const age = 2021 - birthYear
+//     const allowed = 18 - age
+//     return `You can drive ${allowed} year later.`
+// }
+
+// const result = canDrive(2006)
+// console.log(result)
 
 
-/*
-There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
-Your tasks:
+// function hipotenus (a, b){
+//     const firstValue = exponentValues(a)
+//     const secondValue = exponentValues(b)
+//     const result = (firstValue + secondValue) ** (0.5)
+//     return result 
+// }
 
-1. Write a function that calculates the average points of the teams.
-2. Write another function to compare the averages and return the winner of the play.
+// function exponentValues(value){
+//     return value ** 2
+// }
 
-Data 1: Dolphins score: 96, 108 and 89. 
-        Koalas score:  88, 91 and 110
+// const hipoResult = hipotenus (7, 24)
+// console.log(hipoResult)
+
+
+// /*
+// There are two gymnastics teams, Dolphins and Koalas. They compete against each other 3 times. The winner with the highest average score wins a trophy!
+// Your tasks:
+
+// 1. Write a function that calculates the average points of the teams.
+// 2. Write another function to compare the averages and return the winner of the play.
+
+// Data 1: Dolphins score: 96, 108 and 89. 
+//         Koalas score:  88, 91 and 110
+// */
+
+
+// function average (a, b, c){
+//     return (a + b + c) / 3
+// }
+
+// const avgDolphin = average(96, 108, 89)
+// console.log(avgDolphin)
+// const avgKoala = average(88, 91, 110)
+// console.log(avgKoala)
+
+// function findWinner(avgDolphin, avgKoala){
+//     if (avgDolphin > avgKoala){
+//         return `The winner is Dolphin`
+//     }else{
+//         return `The winner is Koala`
+//     }
+// }
+
+// console.log(findWinner(avgDolphin, avgKoala))
+
+/** 
+ Find out if a given year is a “leap” year.
+In the Gregorian calendar, three criteria must be taken into account to identify leap years:
+-- The year must be evenly divisible by 4;
+-- If the year can also be evenly divided by 100, it is not a leap year; unless...
+--  The year is also evenly divisible by 400. Then it is a leap year.
+According to these rules, the years 2000 and 2400 are leap years, while 1800, 1900, 2100, 2200, 2300, and 2500 are not the leap years.
+Write a program that takes the year from user and gives output as “2020 is a leap year” or “2019 is not a leap year” .
+ */
+
+
+// const year = prompt("Enter a year: ")
+
+// function leapYear (year){
+//     if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0){
+//         return `${year} is a leap year.`
+//     }else {
+//         return `${year} is not a leap year.`
+//     }
+// }
+
+// console.log(leapYear(year))
+
+
+/**
+1.Take a positive number from user,
+2. Write a function that check whether  the number is "Armstrong" or not.
+
+An n-digit number that is the sum of the nth powers of its digits is called an n-Armstrong number. Examples :
+371 =3 ** 3 + 7 ** 3 + 1 ** 3;
+9474 = 9 ** 4 + 4 ** 4 + 7 ** 4 + 4 ** 4;
+93084 = 9 ** 5 + 3 ** 5 + 0 ** 5 + 8 ** 5 + 4 **5.
+
+P.S:  consider the negative, decimal and any entries other than numeric values then display a warning message to the user.
+
 */
 
 
-function average (a, b, c){
-    return (a + b + c) / 3
-}
+// const number = prompt("Enter a three digit number: ")
 
-const avgDolphin = average(96, 108, 89)
-console.log(avgDolphin)
-const avgKoala = average(88, 91, 110)
-console.log(avgKoala)
+// function isArmstrong(number){
+//     const len = number.length
+//     const newNumber = number.split("")
+//     console.log(newNumber)
+//     console.log(len)
 
-function findWinner(avgDolphin, avgKoala){
-    if (avgDolphin > avgKoala){
-        return `The winner is Dolphin`
-    }else{
-        return `The winner is Koala`
-    }
-}
+//     const sum = newNumber[0] ** len + newNumber[1] ** len + newNumber[2] ** len
 
-console.log(findWinner(avgDolphin, avgKoala))
+//     return sum == number ? "is Armstrong" :  "not Armstrong"
+// }
+
+// console.log(isArmstrong(number))
+
+// ------------------------------------ STRING METHODS -------------------------------- //
+
+let text = "Her şey güzel olacak !"
+let len = text.length
+console.log("length " + len)
+
+// EXTRACT METHODS 
+// slice(start,end)
+
+let firstPArt = text.slice(8,13)
+console.log(firstPArt)
+console.log("text: " + text)
+
+let secondPart = text.slice(8)
+console.log(secondPart)
+
+let thirdPart = text.slice(-14, -9)
+console.log(thirdPart)
+
+// substring(start, end)
+// does  not accept negative numbers
+let part = text.substring(8, 13)
+console.log(part)
+
+// substr(start, length)
+
+let substrPart = text.substr(8,5)
+console.log(substrPart)
+
+let substrPart2 = text.substr(-14, 5)
+console.log(substrPart)
+
+// SPLIT
+
+let text2 = "Turkey, Germany, USA, France"
+let splittedPart = text2.split(" ")
+console.log(splittedPart)
+
+let splittedPart2 = text2.split(",")
+console.log(splittedPart2)
+//console.log(splittedPart2[1])
+
+let splittedPart3 = text.split("")
+console.log(splittedPart3)
+
+// REPLACE
+let string = "Current euro currency, is 15,22"
+let splitIt = string.split(" ")
+console.log(splitIt)
+let currency = splitIt[splitIt.length - 1]
+
+currency = currency.replace(",", ".")
+console.log(currency)
+// replace method only replaces the first match value.
+currency = Number(currency)
+console.log(currency)
+
+let replacedStr = string.replaceAll(",", ".")
+console.log(replacedStr)
+
+let string2 = "Welcome to DGHome"
+let newString = string2.replace("DGHome", "DigiHome")
+let newString2 = string2.replace(" ", "")
+console.log(newString)
+//console.log(newString2)
+
+// TRIM
+let string3 = "       Welcome to DGHome        "
+console.log(string3)
+let trimmed = string3.trim()
+console.log(trimmed)
+// let replacedAll = string3.replaceAll(" ", "")
+// console.log(replacedAll)
+
+
+// INCLUDES 
+// returns true or false
+console.log(trimmed.includes("Welcome"))
+
+// INDEXOF
+let string4 = "All is well !"
+let ind = string4.indexOf("l")
+let lastInd = string4.lastIndexOf("l")
+console.log(lastInd)
+console.log(ind)
+
+let ind2 = string4.indexOf("l", ind + 1)
+let ind3 = string4.indexOf("l", ind2 + 1)
+console.log(ind2)
+console.log(ind3)
+
+// concat
+let s1 = "Hello "
+let s2 = "World"
+let s3 = s1.concat(s2)
+console.log(s3)
+
+//charAt
+let char = s3.charAt(6)
+console.log(char)
+
+
+// toLowerCase / toUpperCase
+let newStr = string3.toLowerCase()
+console.log(newStr) 
+let newStr2 = string3.toUpperCase()
+console.log(newStr2) 
+// ------------------------------------ STRING METHODS -------------------------------- //
